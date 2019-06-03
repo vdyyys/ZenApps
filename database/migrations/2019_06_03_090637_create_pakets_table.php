a76fb5b2a32e84548deeddf44a637146b3fc298a
likeJelly <jalluramadhan@gmail.com>
2019-06-03 16:16:03 +0700

    Paket

---
+++ database/migrations/2019_06_03_090637_create_pakets_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePaketsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pakets', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('id_eo');
+            $table->string('gambar_paket');
+            $table->string('nama_paket');
+            $table->string('kategori');
+            $table->string('available');
+            $table->text('deskripsi');
+            $table->bigInteger('harga_paket');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pakets');
+    }
+}
